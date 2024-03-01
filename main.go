027c7e1fa66c224dd8c2c0031a57d021a020a83f
maria.eugenia <mariugiraldo40@gmail.com>
2024-03-01 15:06:46 -0500

    requerimiento 4

--- main.go
+++ main.go
@@ -2,16 +2,46 @@ package main
 
 import (
 	
+	"fmt"
 
 	"github.com/bootcamp-go/desafio-go-bases/internal/tickets"
 )
 
 func main() {
 
-	 tickets.GetTotalTickets("Japan")
+	totalTicketsChan := make(chan int)
+    passengerCountChan := make(chan map[string]int)
+   	averageChan := make(chan float64)
 
-	 tickets.GetPassengersByTimeOfDay()
+	go func() {
+        total, _ := tickets.GetTotalTickets("Brazil")
+        totalTicketsChan <- total 
+    }()
+	go func() {
+        passengerCount := tickets.GetPassengersByTimeOfDay()
+        passengerCountChan <- passengerCount 
+    }()
 
-	 tickets.GetAverageDestination("Japan")
-	
+	go func() {
+        averange, _ := tickets.GetAverageDestination("Brazil")
+        averageChan <- averange 
+    }()
+
+	totalTickets := <-totalTicketsChan
+    fmt.Printf("Total de tickets para Japan: %d\n", totalTickets)
+
+    passengerCount := <-passengerCountChan
+    fmt.Println("Cantidad de pasajeros por franja horaria:")
+    for key, value := range passengerCount {
+        fmt.Printf("%s: %d\n", key, value)
+    }
+
+	average := <-averageChan
+    fmt.Printf("Porcentaje de personas que viajan a Brazil: %.2f%%\n", average)
+
+
+	close(totalTicketsChan)
+    close(passengerCountChan)
+    close(averageChan)
 }
+	
