a551df1fa7ecfcb4a7dac845d5a4c5127cc25cc7
maria.eugenia <mariugiraldo40@gmail.com>
2024-03-01 15:36:53 -0500

    ajustes

--- internal/tickets/tickets.go
+++ internal/tickets/tickets.go
@@ -20,7 +20,7 @@ type Ticket struct {
 func loadTickets() []Ticket {
 	data, err := os.ReadFile("./tickets.csv")
 	if err != nil {
-		return nil
+		panic(err)
 	}
 
 	lines := strings.Split(string(data), "\n")
@@ -45,16 +45,14 @@ func loadTickets() []Ticket {
 			}
 			ticketList = append(ticketList, ticket)
 		}
-		
+
 	}
 
-
 	return ticketList
-	
+
 }
 
-
-// Requerimiento 1
+// Requerimiento 1: Una función que calcule cuántas personas viajan a un país determinado.
 func GetTotalTickets(destination string) (int, error) {
 	var ticketList = loadTickets()
 
@@ -66,87 +64,73 @@ func GetTotalTickets(destination string) (int, error) {
 		}
 
 	}
-	fmt.Println("Total de tickets para " + destination,  total)
-
+	fmt.Println("Total de tickets para "+destination, total)
 
 	return total, nil
 }
 
-// Requerimiento 2
-func GetPassengersByTimeOfDay()map [string] int {
-	
+// Requerimiento 2: Una o varias funciones que calculen cuántas personas viajan en madrugada (0 → 6), mañana (7 → 12), tarde (13 → 19), y noche (20 → 23).
+func GetPassengersByTimeOfDay() map[string]int {
+
 	ticketList := loadTickets()
 
-    var madrugadaCount, mañanaCount, tardeCount, nocheCount int
+	var madrugadaCount, mañanaCount, tardeCount, nocheCount int
 
 	for _, ticket := range ticketList {
-        flightTime, err := time.Parse("15:04", ticket.FlightTime)
-        if err != nil {
-            fmt.Println("Error al analizar la hora de vuelo:", err)
-            continue
-        }
+		flightTime, err := time.Parse("15:04", ticket.FlightTime)
+		if err != nil {
+			fmt.Println("Error al analizar la hora de vuelo:", err)
+			continue
+		}
 
-        hour := flightTime.Hour()
-        if hour >= 0 && hour < 6 {
-            madrugadaCount++
-        } else if hour >= 6 && hour < 12 {
-            mañanaCount++
-        } else if hour >= 12 && hour < 20 {
-            tardeCount++
-        } else {
-            nocheCount++
-        }
-    }
+		hour := flightTime.Hour()
+		if hour >= 0 && hour < 6 {
+			madrugadaCount++
+		} else if hour >= 6 && hour < 12 {
+			mañanaCount++
+		} else if hour >= 12 && hour < 20 {
+			tardeCount++
+		} else {
+			nocheCount++
+		}
+	}
 
-    passengerCountByTimeOfDay := map[string]int{
-        "Madrugada": madrugadaCount,
-        "Mañana":    mañanaCount,
-        "Tarde":     tardeCount,
-        "Noche":     nocheCount,
-    }
+	passengerCountByTimeOfDay := map[string]int{
+		"Madrugada": madrugadaCount,
+		"Mañana":    mañanaCount,
+		"Tarde":     tardeCount,
+		"Noche":     nocheCount,
+	}
 
-    fmt.Println("Cantidad de pasajeros por franja horaria:")
-    for key, value := range passengerCountByTimeOfDay {
-        fmt.Printf("%s: %d\n", key, value)
-    }
+	fmt.Println("Cantidad de pasajeros por franja horaria:")
+	for key, value := range passengerCountByTimeOfDay {
+		fmt.Printf("%s: %d\n", key, value)
+	}
 
-    return passengerCountByTimeOfDay
+	return passengerCountByTimeOfDay
 }
 
-
-// Requerimiento  3
+// Requerimiento 3: Calcular el porcentaje de personas que viajan a un país determinado en un día.
 func GetAverageDestination(destination string) (float64, error) {
 	ticketList := loadTickets()
 
 	DestinationTickets := 0
 	TotalTickets := 0
 
-	for _, ticket := range ticketList{
-		if strings.EqualFold(ticket.DestinationCountry, destination){
-			DestinationTickets ++
+	for _, ticket := range ticketList {
+		if strings.EqualFold(ticket.DestinationCountry, destination) {
+			DestinationTickets++
 		}
-		TotalTickets ++
+		TotalTickets++
 	}
 
 	if TotalTickets == 0 {
 		return 0, fmt.Errorf("No se encontraron tickets")
 	}
-	
-	average := (float64 (DestinationTickets) / float64 (TotalTickets)) * 100
 
-	fmt.Printf("Porcentaje de personas que viajan a %s: %.2f%%\n", destination, average, )
+	average := (float64(DestinationTickets) / float64(TotalTickets)) * 100
+
+	fmt.Printf("Porcentaje de personas que viajan a %s: %.2f%%\n", destination, average)
 
 	return average, nil
 }
-
-
-
-
-
-
-
-
-
-
-
-	
--- main.go
+++ main.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	
 	"fmt"
 
 	"github.com/bootcamp-go/desafio-go-bases/internal/tickets"
@@ -10,38 +9,43 @@ import (
 func main() {
 
 	totalTicketsChan := make(chan int)
-    passengerCountChan := make(chan map[string]int)
-   	averageChan := make(chan float64)
+	passengerCountChan := make(chan map[string]int)
+	averageChan := make(chan float64)
 
 	go func() {
-        total, _ := tickets.GetTotalTickets("Brazil")
-        totalTicketsChan <- total 
-    }()
+		total, _ := tickets.GetTotalTickets("Brazil")
+		totalTicketsChan <- total
+	}()
 	go func() {
-        passengerCount := tickets.GetPassengersByTimeOfDay()
-        passengerCountChan <- passengerCount 
-    }()
+		passengerCount := tickets.GetPassengersByTimeOfDay()
+		passengerCountChan <- passengerCount
+	}()
 
 	go func() {
-        averange, _ := tickets.GetAverageDestination("Brazil")
-        averageChan <- averange 
-    }()
+		averange, _ := tickets.GetAverageDestination("Brazil")
+		averageChan <- averange
+	}()
+
+	defer func() {
+		if r := recover(); r != nil {
+			fmt.Println("Ocurrió un error:", r)
+		}
+	}()
 
 	totalTickets := <-totalTicketsChan
-    fmt.Printf("Total de tickets para Japan: %d\n", totalTickets)
+	fmt.Printf("Total de tickets para Japan: %d\n", totalTickets)
 
-    passengerCount := <-passengerCountChan
-    fmt.Println("Cantidad de pasajeros por franja horaria:")
-    for key, value := range passengerCount {
-        fmt.Printf("%s: %d\n", key, value)
-    }
+	passengerCount := <-passengerCountChan
+	fmt.Println("Cantidad de pasajeros por franja horaria:")
+	for key, value := range passengerCount {
+		fmt.Printf("%s: %d\n", key, value)
+	}
 
 	average := <-averageChan
-    fmt.Printf("Porcentaje de personas que viajan a Brazil: %.2f%%\n", average)
-
+	fmt.Printf("Porcentaje de personas que viajan a Brazil: %.2f%%\n", average)
 
 	close(totalTicketsChan)
-    close(passengerCountChan)
-    close(averageChan)
+	close(passengerCountChan)
+	close(averageChan)
+
 }
-	
